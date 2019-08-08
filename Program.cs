af59722ec8f37b639b40d8a469d7de49a8ce6108
fasberry <51687090+fasberry@users.noreply.github.com>
2019-08-08 07:47:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 10;
+            var c = 2;
+
+            Console.WriteLine("Enter Number");
+            Console.WriteLine(a + b + c);
+            Console.WriteLine((a + b + c) / 3);
+                        
+        }
+    }
+}
